b832663e05931ac4133887ef04d9f8e8c2de942a
Mohammad Amin Khashkhashi Moghaddam <amin.moghaddamV@gmail.com>
2014-10-11 22:06:49 +0330

    Added templatetags feature Now you can format Jalali dates and times using jformat filter

---
+++ django_jalali/templatetags/jformat.py
@@ -0,0 +1,19 @@
+from django.template.base import Library
+
+register = Library()
+
+
+@register.filter(expects_localtime=True, is_safe=False)
+def jformat(value, arg=None):
+    """Formats a date or time according to the given format."""
+    if value in (None, ''):
+        return ''
+    if arg is None:
+        arg = "%c"
+    try:
+        # this should be force_text but because jdatetime module didn't handle
+        # unicode strings correctly it's not possible to change it at the moment
+        arg = str(arg)
+        return value.strftime(arg)
+    except AttributeError:
+        return ''
