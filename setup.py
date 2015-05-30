be6963a1845a465230259d3dee915621d70de23a
Milad Rastian <milad@sam-media.com>
2015-05-30 19:16:28 +0800

    Oops!

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='django-jalali',
-        version='1.5',
+        version='1.1',
         packages=find_packages(),
         include_package_data=True,
         zip_safe=False,
