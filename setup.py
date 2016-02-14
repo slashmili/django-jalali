6d9a63fee008340037f2ba59873ea0ec63f80987
Milad Rastian <milad@sam-media.com>
2016-02-14 19:27:47 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='django-jalali',
-        version='2.3.2',
+        version='2.3.3',
         packages=find_packages(),
         include_package_data=True,
         zip_safe=False,
