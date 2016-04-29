0f1640fc9296304d9241a57aa740ac45517d552a
Milad Rastian <eslashmili@gmail.com>
2016-04-30 00:00:50 +0800

    Implement JDateFieldListFilter to filter dates in admin panel

--- jalali_test/foo/admin.py
+++ jalali_test/foo/admin.py
@@ -1,3 +1,22 @@
+from foo.models import Bar, BarTime
 from django.contrib import admin
 
-# Register your models here.
+from django_jalali.admin.filters import JDateFieldListFilter
+
+#you need import this for adding jalali calander widget
+import django_jalali.admin as jadmin
+
+class BarAdmin(admin.ModelAdmin):
+    list_filter = (
+        ('date', JDateFieldListFilter),
+    )
+    pass
+
+admin.site.register(Bar, BarAdmin)
+
+class BarTimeAdmin(admin.ModelAdmin):
+    list_filter = (
+        ('datetime', JDateFieldListFilter),
+    )
+
+admin.site.register(BarTime, BarTimeAdmin)
--- jalali_test/jalali_test/settings.py
+++ jalali_test/jalali_test/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_jalali',
     'foo',
 )
 
@@ -94,7 +95,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
