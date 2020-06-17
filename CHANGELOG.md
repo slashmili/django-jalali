e9174d312ca2d2436c784085f23f13313d9cf5aa
Milad Rastian <milad@high-mobility.com>
2020-06-17 05:05:04 +0200

    prepreation of release 3.4.0

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## [3.4.0] - 2020-06-17
+### Fixed
+- Fix serialization of django_jalali.db.models
+
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-jalali',
-    version='3.3.0',
+    version='3.4.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
