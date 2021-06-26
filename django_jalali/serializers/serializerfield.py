b7ba2c4933567804e10b33d121fc286cef3afce0
ahmad98 <ahmadmansooripv@gmail.com>
2021-06-26 05:59:53 -0700

    add serializer field for jDateTime field + change parse_date to class method

--- django_jalali/serializers/serializerfield.py
+++ django_jalali/serializers/serializerfield.py
@@ -1,4 +1,5 @@
 import jdatetime
+from django.core import exceptions
 from rest_framework import serializers
 
 from django_jalali.db.models import jDateField
@@ -16,5 +17,24 @@ class JDateField(serializers.DateField):
         return jDateField.parse_date(value)
 
     # This method is used by DRF to bringing value back to python form
+    def to_internal_value(self, value):
+        return self.to_python(value)
+
+
+class JDateTimeField(serializers.DateTimeField):
+    def to_python(self, value):
+        if value is None:
+            return value
+
+        if isinstance(value, jdatetime.datetime):
+            return value
+        if isinstance(value, jdatetime.date):
+            try:
+                return jdatetime.datetime(value.year, value.month, value.day)
+            except ValueError:
+                raise exceptions.ValidationError(
+                    self.error_messages['invalid'])
+        return JDateTimeField.parse_date(value)
+
     def to_internal_value(self, value):
         return self.to_python(value)
