fd4246ddd1254f567ff1f60a382e1a428bf48318
ahmad98 <ahmadmansooripv@gmail.com>
2021-07-01 14:31:55 +0200

    Add DRF field for jmodels.JDateTimeField.
    Co-authored-by: Hasan Ramezani <hasan.r67@gmail.com>

--- django_jalali/serializers/serializerfield.py
+++ django_jalali/serializers/serializerfield.py
@@ -1,7 +1,10 @@
 import jdatetime
+from django.core import exceptions
 from rest_framework import serializers
 
-from django_jalali.db.models import jDateField
+from django_jalali.db.models import (
+    jDateField as jDateFieldModel, jDateTimeField as jDateTimeFieldModel,
+)
 
 
 class JDateField(serializers.DateField):
@@ -13,8 +16,33 @@ class JDateField(serializers.DateField):
             return value.date()
         if isinstance(value, jdatetime.date):
             return value
-        return jDateField.parse_date(value)
+        return jDateFieldModel.parse_date(value)
 
     # This method is used by DRF to bringing value back to python form
     def to_internal_value(self, value):
         return self.to_python(value)
+
+
+class JDateTimeField(serializers.DateTimeField):
+    def to_python(self, value):
+        if value is None:
+            return value
+
+        if isinstance(value, jdatetime.datetime):
+            return value
+        if isinstance(value, jdatetime.date):
+            try:
+                return jdatetime.datetime(value.year, value.month, value.day)
+            except ValueError:
+                raise exceptions.ValidationError(
+                    self.error_messages['invalid'])
+        return jDateTimeFieldModel.parse_date(value)
+
+    # This method is used by DRF to bringing value back to python form
+    def to_internal_value(self, value):
+        return self.to_python(value)
+
+    # This method is used by DRF for representing object in string form
+    # override this function with strftime if needed
+    def to_representation(self, value):
+        return str(value)
