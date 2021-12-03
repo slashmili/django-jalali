f6ea84924044fc26aa9421c0fbb11f945a89d1cb
Hasan Ramezani <hasan.r67@gmail.com>
2021-12-03 20:49:58 +0100

    Add Python 3.10 support.

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython",
         'Framework :: Django',
         'Framework :: Django :: 2.2',
