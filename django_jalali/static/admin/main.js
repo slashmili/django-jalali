be8d97de914d2c8fb2f15e3e09c76e794fc8d7d4
Mobin Ghoveoud <mobin.sgh@gmail.com>
2024-10-22 17:07:31 +0330

    Fix datepicker for inline admin (#263)
    fix #255

--- django_jalali/static/admin/main.js
+++ django_jalali/static/admin/main.js
@@ -1,11 +1,32 @@
 $(function () {
+    function activateDatepicker() {
+        $('.vjDateField').each(function() {
+            var inputId = $(this).attr('id') || '';
+            if (inputId.includes('__prefix__')) {
+                return;
+            }
+
+            $(this).datepicker({
+                dateFormat: "yy-mm-dd",
+                changeMonth: true,
+                changeYear: true,
+                showOn: "both",
+                buttonImage: "/static/admin/jquery.ui.datepicker.jalali/themes/base/images/icon-calendar.svg",
+                buttonImageOnly: true,
+                isRTL: false,
+                buttonText: "یک تاریخ انتخاب کنید",
+            });
+        });
+    }
+
     if (!$.datepicker){$ = django.jQuery;}
-    $('.vjDateField').datepicker({
-        dateFormat: 'yy-mm-dd',
-        changeMonth: true,
-        changeYear: true,
-        showOn: 'button',
-        buttonImage: '/static/admin/jquery.ui.datepicker.jalali/themes/base/images/icon-calendar.svg',
-        buttonImageOnly: true
+    activateDatepicker();
+
+    $(document).on('formset:added', function(event, $row, formsetName) {
+        activateDatepicker();
+    });
+
+    $(document).on('formset:removed', function(event, $row, formsetName) {
+        activateDatepicker();
     });
 });
